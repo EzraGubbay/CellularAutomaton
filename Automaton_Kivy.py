7785048a0477c35c6a362cac5209810a8c3385bd
Ezra Gubbay <ezragubbay99@gmail.com>
2025-04-23 10:08:26 +0300

    LogicManager update bug

--- Automaton_Kivy.py
+++ Automaton_Kivy.py
@@ -21,9 +21,15 @@ TODO:
         a specific initial state (the setting of the initial state is handled by
         LogicManager)
 '''
-import random
-from curses.textpad import rectangle
-from time import sleep
+from http.cookiejar import cut_port_re
+
+from kivy.config import Config
+from pygments.lexers import j
+
+# Set the initial window position BEFORE importing any UI modules
+Config.set('graphics', 'position', 'custom')
+Config.set('graphics', 'left', '100')   # X coordinate from the left of the screen
+Config.set('graphics', 'top', '50')    # Y coordinate from the top of the screen
 
 from kivy.app import App
 from kivy.lang.builder import Builder
@@ -36,7 +42,6 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.graphics.vertex_instructions import Rectangle, Line
 from kivy.uix.widget import Widget
 
 from LogicManager import LogicManager
@@ -57,22 +62,25 @@ class Cell(Widget):
     def set_color(self, color):
         self.color = color
 
+    def get_color(self):
+        return self.color
 
 class GameScreen(Screen):
     def __init__(self, dimension: int, logic: LogicManager, **kwargs):
         super().__init__(**kwargs)
 
-        self.grid = []
+        self.grid: [[Cell]] = []
         self.grid_size = dimension
         self.logic = logic
         self.playing = False
 
-        self.main_layout = BoxLayout(orientation="horizontal")
+        self.main_layout = BoxLayout(orientation="horizontal", spacing=dp(50))
 
         # Game Controls
         self.controls = BoxLayout(
             orientation="vertical",
-            size_hint=(0.2, 1),
+            size_hint=(None, 1),
+            size=(dp(200), dp(100)),
             padding=dp(10),
             spacing=dp(10)
         )
@@ -113,17 +121,30 @@ class GameScreen(Screen):
         # Add Controls to the Game Screen
         self.main_layout.add_widget(self.controls)
 
+        # # Horizontal Spacer between controls and grid
+        # self.main_layout.add_widget(BoxLayout(size_hint=(0.2, 0.4)))
+
         # Create grid container
         self.grid_container = GridLayout(
-            size_hint=(0.8, 1)
+            size_hint=(None, None),
+            size=(dp(856), dp(856)),
+            padding=dp(20),
+            rows=self.grid_size,
+            orientation="lr-tb",
         )
         self.main_layout.add_widget(self.grid_container)
+
+        # Create iteration label and generation label
+        self.iteration_label = Label(text="Iteration: 1", font_size=dp(20), font_name=FONT_PATH)
+        self.generation_label = Label(text="Generation: Blue", font_size=dp(20), font_name=FONT_PATH)
+
         self.populate_grid()
+        self.print_grid()
 
         self.add_widget(self.main_layout)
 
     def populate_grid(self):
-        cell_size = 15
+        cell_size = 50 # Original is 15
         placement = (800, 100)
 
         for i in range(self.grid_size):
@@ -140,9 +161,13 @@ class GameScreen(Screen):
 
     def play(self, *args):
         self.playing = True
-        for i in range(250):
-            self.logic.update()
-            self.update_grid()
+        self.logic.update()
+        self.update_grid()
+        self.print_grid()
+        current_iter = self.logic.iteration
+        self.iteration_label.text = f"Iteration: {current_iter}"
+        # Generation is blue if iteration is odd, else is red.
+        self.generation_label.text = f'Generation: {"Blue" if current_iter % 2 == 1 else "Red" }'
 
     def update_grid(self):
         for i in range(self.grid_size):
@@ -152,10 +177,26 @@ class GameScreen(Screen):
                     else (0, 0, 0, 1)
                 )
 
+    def print_grid(self):
+        print(self.grid)
+        print()
+        print("--- START AUTOMATON_KIVY ---")
+        print("[*] Logic Grid: ")
+        for i in range(self.grid_size):
+            states = [self.logic.main_matrix[i][j].get_state() for j in range(self.grid_size)]
+            print(states)
+        print()
+        print("[*] Graphical Grid: ")
+        for i in range(self.grid_size):
+            states = [1 if self.grid[i][j].color == [1, 0, 0, 1] else 0 for j in range(self.grid_size)]
+            print(states)
+        print()
+        print("--- END AUTOMATON_KIVY ---")
+
 class WindowManager(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.dimension = 10
+        self.dimension = 2 # Original is 100
         self.logic = LogicManager(dimension=self.dimension, wraparound=False)
         gs = GameScreen(dimension=self.dimension, logic=self.logic)
         self.add_widget(gs)
@@ -165,7 +206,8 @@ Window.size = (1280, 896)
 
 class AutomatonApp(App):
     def build(self):
-        return kv
+        wm = WindowManager()
+        return wm
 
 
 
--- LogicManager.py
+++ LogicManager.py
@@ -1,7 +1,7 @@
 
 ### TODO - LogicManager
-# TODO: Change hardcoded n value in initializer to received input n.
-# TODO: Add wraparound boolean input in initializer.
+# TODO: Change hardcoded n value in initializer to received input n. DONE
+# TODO: Add wraparound boolean input in initializer. DONE
 # TODO: Build Red Matrix appropriately for wraparound mode.
 
 from Cell import Cell
@@ -14,26 +14,89 @@ class LogicManager:
         # Hard-coded 10, in the future the size of the matrices
         # will be based on an input n.
         n = dimension
-        self._generation = 1
+        self.wraparound = wraparound
+        self.iteration = 1
         self.main_matrix = [[Cell() for i in range(n)] for j in range(n)]
         self.blue_matrix = self._blue_matrix_builder(n)
         self.red_matrix = self._red_matrix_builder(n)
 
     # Logic methods
+    # def update(self):
+    #     # Determine generation type
+    #     generation_type: int = self.generation % 2
+    #
+    #     # If the generation is odd, update according to logic on blue's blocks, otherwise on red's.
+    #     matrix = self.blue_matrix if generation_type == 1 else self.red_matrix
+    #     is_red = int(not generation_type)
+    #     for i, row in enumerate(matrix):
+    #         for j, block in enumerate(row):
+    #
+    #             # DEBUG #
+    #             print("--- LogicManager ---")
+    #             print("Indexes | I: ", i, "| J:", j)
+    #             print("2I + 1: ", 2*i + 1, "| 2J + 1: ", 2*j + 1)
+    #
+    #             block.update()
+    #
+    #             # Update references to cells, in case a rotation occurred in-block.
+    #             x = 2 * i + is_red
+    #             y = 2 * j + is_red
+    #             self.main_matrix[x][y] = block.cells[0]
+    #             self.main_matrix[x][y + 1] = block.cells[1]
+    #             self.main_matrix[x + 1][y] = block.cells[2]
+    #             self.main_matrix[x + 1][y + 1] = block.cells[3]
+    #
+    #     # At the end of the update, increment the generation number.
+    #     print("Generation Type: ", generation_type)
+    #     print("Is Red: ", is_red)
+    #     print("--- LogicManager --- END UPDATE")
+    #     self.generation += 1
+
     def update(self):
-        generation_type: int = self._generation % 2
-        if generation_type == 0: # Even generation, check blue blocks.
-            for row in self.blue_matrix:
-                for block in row:
-                    block.update()
+        generation_type = 1 if self.iteration % 2 == 1 else 0
+        if generation_type == 1:
+            self._update_blue()
         else:
-            for row in self.red_matrix:
-                for block in row:
-                    block.update()
+            self._update_red()
+        self.iteration += 1
+
+    def _update_blue(self):
+        for i in range(0, len(self.main_matrix), 2):
+            for j in range(0, len(self.main_matrix), 2):
+                self.update_block(i, j)
+
+    def _update_red(self):
+        print("updating red")
+        for i in range(1, len(self.main_matrix) - 1, 2):
+            for j in range(1, len(self.main_matrix) - 1, 2):
+                self.update_block(i, j)
+
+    def update_block(self, i, j):
+        block_state = (
+                self.main_matrix[i][j].get_state() +
+                self.main_matrix[i][j + 1].get_state() +
+                self.main_matrix[i + 1][j].get_state() +
+                self.main_matrix[i + 1][j + 1].get_state()
+        )
+
+        if block_state != 2:
+            # Flip states
+            self.main_matrix[i][j].set_state(1 if self.main_matrix[i][j].get_state() == 0 else 0)
+            self.main_matrix[i][j + 1].set_state(1 if self.main_matrix[i][j + 1].get_state() == 0 else 0)
+            self.main_matrix[i + 1][j].set_state(1 if self.main_matrix[i + 1][j].get_state() == 0 else 0)
+            self.main_matrix[i + 1][j + 1].set_state(1 if self.main_matrix[i][j + 1].get_state() == 0 else 0)
+
+            if block_state == 3:
+                temp = self.main_matrix[i][j]
+                self.main_matrix[i][j] = self.main_matrix[i + 1][j + 1]
+                self.main_matrix[i + 1][j + 1] = temp
+                temp = self.main_matrix[i][j + 1]
+                self.main_matrix[i][j + 1] = self.main_matrix[i + 1][j]
+                self.main_matrix[i + 1][j] = temp
 
     ### NOT USED ###
     def _generation_type(self):
-        return "even" if self._generation % 2 == 0 else "odd"
+        return "even" if self.iteration % 2 == 0 else "odd"
 
     # Auxiliary methods - for code readability and modularity.
     def _block_builder(self, i: int, j: int):
@@ -48,4 +111,4 @@ class LogicManager:
         return [[self._block_builder(i, j) for j in range(0, n, 2)] for i in range(0, n, 2)]
 
     def _red_matrix_builder(self, n: int):
-        return [[self._block_builder(i, j) for i in range(1, n - 1)] for j in range(1, n - 1)]
+        return [[self._block_builder(i, j) for i in range(1, n - 1, 2)] for j in range(1, n - 1, 2)]
